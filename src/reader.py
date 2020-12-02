e0fb70d345d9f3e2df2892f33b26732680c187df
Vincent Wong <vincent@vwong.dev>
2020-12-02 16:37:14 +1100

    Update reader function to use generic types

--- src/reader.py
+++ src/reader.py
@@ -1,5 +1,7 @@
-from typing import List, Callable
+from typing import List, Callable, Any, TypeVar
+
+T = TypeVar("T")
 
 
-def reader(file_path: str, func: Callable) -> List[int]:
+def reader(file_path: str, func: Callable[..., T]) -> List[T]:
     return [func(x) for x in list(open(file_path))]
