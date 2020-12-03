c5fb0650415f6f381ec32dc6c4bc301dd68bad3b
Vincent Wong <vincent@vwong.dev>
2020-12-03 17:14:39 +1100

    Solve day 3 problems

---
+++ src/day3.py
@@ -0,0 +1,26 @@
+from typing import List
+
+import math
+
+from reader import reader
+
+
+def count_trees(tree_map: List[str], right: int, down: int) -> int:
+    count = 0
+
+    for i in range(len(tree_map) // down):
+        y = i * down
+        x = (right * i) % (len(tree_map[0]) - 1)
+
+        count += tree_map[y][x] == "#"
+
+    return count
+
+
+if __name__ == "__main__":
+    data = reader("./input.txt", str)
+
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    tree_counts = [count_trees(data, right, down) for right, down in slopes]
+    print(math.prod(tree_counts))
