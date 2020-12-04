a3fa224596369db03a02c5c2eaf0c7f7f7db4d38
Vincent Wong <vincent@vwong.dev>
2020-12-05 01:41:17 +1100

    Remove code duplication in day 4 solutions

--- src/day4.py
+++ src/day4.py
@@ -5,13 +5,22 @@ import re
 from reader import reader
 
 
-FIELDS = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+RULES = {
+    "byr": lambda year: 1920 <= int(year) <= 2002,
+    "iyr": lambda year: 2010 <= int(year) <= 2020,
+    "eyr": lambda year: 2020 <= int(year) <= 2030,
+    "hgt": lambda height: (height[-2:] == "cm" and 150 <= int(height[:-2]) <= 193) \
+                            or (height[-2:] == "in" and 59 <= int(height[:-2]) <= 76),
+    "hcl": lambda colour: len(colour) == 7 and colour[0] == '#' and colour[1:].isalnum(),
+    "ecl": lambda colour: colour in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
+    "pid": lambda id: len(id) == 9 and id.isnumeric(),
+}
 
 
 def parse_passport(raw_passport: str) -> Dict[str,str]:
     passport = {}
 
-    for field in FIELDS:
+    for field in RULES:
         if match := re.match(f".*{field}:([\S]+)", raw_passport):
             passport[field] = match.group(1)
 
@@ -19,22 +28,11 @@ def parse_passport(raw_passport: str) -> Dict[str,str]:
 
 
 def is_valid_passport1(passport: Dict[str, str]) -> bool:
-    return all(field in passport for field in FIELDS)
+    return all(field in passport for field in RULES)
 
 
 def is_valid_passport2(passport: Dict[str, str]) -> bool:
-    rules = {
-        "byr": lambda year: 1920 <= int(year) <= 2002,
-        "iyr": lambda year: 2010 <= int(year) <= 2020,
-        "eyr": lambda year: 2020 <= int(year) <= 2030,
-        "hgt": lambda height: (height[-2:] == "cm" and 150 <= int(height[:-2]) <= 193) \
-                              or (height[-2:] == "in" and 59 <= int(height[:-2]) <= 76),
-        "hcl": lambda colour: len(colour) == 7 and colour[0] == '#' and colour[1:].isalnum(),
-        "ecl": lambda colour: colour in ("amb", "blu", "brn", "gry", "grn", "hzl", "oth"),
-        "pid": lambda id: len(id) == 9 and id.isnumeric(),
-    }
-
-    return all(key in passport and rules[key](passport[key]) for key in FIELDS)
+    return all(key in passport and RULES[key](passport[key]) for key in RULES)
 
 
 if __name__ == "__main__":
